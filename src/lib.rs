3f1cd8e79bd52b0b399c2c5c84bc024d56a93188
GustavEkberg <krypteratkadaver@gmail.com>
2023-03-30 13:51:05 +0200

    initial commit

---
+++ src/lib.rs
@@ -0,0 +1,16 @@
+pub mod content;
+
+#[cfg(test)]
+mod tests {
+    use crate::content::extract_url_content;
+
+    #[tokio::test]
+    async fn it_works() {
+        let result = extract_url_content("https://levelup.gitconnected.com/are-programmers-getting-replaced-by-ai-gpt-4s-top-strategies-to-future-proof-your-coding-career-a5a2d4ba95ed")
+            .await
+            .unwrap();
+
+        dbg!(result.clone().unwrap());
+        assert_ne!(result, None);
+    }
+}
